63a76f3b4a20854cb40202aa97e4229976959ae3
Divykumar2003 <88126528+Divykumar2003@users.noreply.github.com>
2022-09-19 13:12:13 +0530

    Update PatientController.java

--- src/main/java/com/example/demo/controller/PatientController.java
+++ src/main/java/com/example/demo/controller/PatientController.java
@@ -3,7 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.model.Patient;
 import com.example.demo.repository.PatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*; 
 
 import java.util.List;
 
